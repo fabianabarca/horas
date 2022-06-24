a102cc8027942fa5a75388f128d52272eb9da131
jmtv <jmtvlu@gmail.com>
2022-06-24 14:05:39 -0600

    migrations

--- actividades/models.py
+++ actividades/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from proyectos.models import Proyecto
+from tareas.models import Tarea
 from cuentas.models import Estudiante
 from django.db import models
 from django.conf import settings
@@ -15,6 +16,8 @@ class Actividad(models.Model):
     )
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+
     descripcion = models.CharField(max_length=500)
     fecha = models.DateField()
     horas = models.IntegerField()
