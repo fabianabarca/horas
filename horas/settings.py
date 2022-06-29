918d978966d2a33b38b7f9de049d9dad02ce321a
Tropicalización <tropicalizacion@ucr.ac.cr>
2022-06-29 21:27:48 +0000

    Agregar la IP del servidor de prueba.

--- horas/settings.py
+++ horas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#(cc)eq+&ml&%0v6fcgyhts+&9fsdl)u9*&k0*uo)zw+&$nxh1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['146.190.48.253']
+ALLOWED_HOSTS = ['146.190.48.253', 'localhost']
 
 
 # Application definition
