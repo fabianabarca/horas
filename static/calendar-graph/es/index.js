6a68f52174148311f7521d3b96c1c057c739c7de
jmtv <jmtvlu@gmail.com>
2022-07-03 11:32:46 -0600

    removiendo seguimiento de algunos archivos

--- static/calendar-graph/es/index.js
+++
@@ -1,5 +0,0 @@
-import SVGGraph from './SVGGraph';
-import CanvasGraph from './CanvasGraph';
-import StrGraph from './StrGraph';
-export default CanvasGraph;
-export { SVGGraph, CanvasGraph, StrGraph };
