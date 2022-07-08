c9e3176d63104a84cdb7eaaba5873289810e0a4b
jmtv <jmtvlu@gmail.com>
2022-07-07 18:17:51 -0600

    barra de progreso reacciona a cambios en las horas

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import get_object_or_404, render
 from dashboard.models import *
 from django.contrib.auth.decorators import login_required
 import time
+from cuentas.models import *
+from actividades.models import Actividad
 
 # Create your views here.
 @login_required(login_url='/cuentas/login/')
@@ -16,4 +18,22 @@ def dashboard_request(request):
                # obj = Tarea( id = deleteButtonItemValue[0]) 
                 #Tarea.objects.filter(id = deleteButtonItemValue[0]).update(enPapelera='True')
 
-    return render (request=request, template_name="../templates/dashboard.html", context={})
+    estudiantes_list = Estudiante.objects.all()
+    estudiante_actual = Estudiante.objects.get(user = request.user)
+
+    #if request.user.is_staff:
+    actividades_list = Actividad.objects.all()
+
+    Actividad.objects.raw('SELECT id, horas FROM myapp_actividad where id == '+ str(estudiante_actual.id))
+    horasTotalesPorEstudiante=0      
+
+    for actividad in actividades_list:
+            horasTotalesPorEstudiante+= actividad.horas
+
+    #horasTotalesPorEstudiante=2   #para pruebas
+    porcentaje= (100 / 300) * horasTotalesPorEstudiante
+    porcentajeWidth = int(porcentaje)
+    print(horasTotalesPorEstudiante)
+    print(porcentaje)
+
+    return render (request=request, template_name="../templates/dashboard.html", context={"progreso":horasTotalesPorEstudiante,"porcentaje":porcentaje,"width":porcentajeWidth,})
