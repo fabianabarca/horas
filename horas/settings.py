c4ff62b7c7dda7fb03ce2d03f53adebe4210a96a
jmtv <jmtvlu@gmail.com>
2022-07-16 12:18:27 -0600

    app pass

--- horas/settings.py
+++ horas/settings.py
@@ -166,7 +166,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'testertesrter3@gmail.com'
-EMAIL_HOST_PASSWORD = 'bwjfhwdsqwdnfwcv'
+EMAIL_HOST_PASSWORD = 'xqemaiswwdlqmsdk'
 
 # Custom setting. To email
 RECIPIENT_ADDRESS = 'testertesrter3@gmail.com'
