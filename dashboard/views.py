450dbff4b53ad481c07c137eb1c55a505b852224
jmtv <jmtvlu@gmail.com>
2022-07-17 23:56:13 -0600

    db change

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,33 +7,42 @@ from django.contrib.auth.decorators import login_required
 import time
 from cuentas.models import *
 from actividades.models import Actividad
+import datetime
+
 
 # Create your views here.
 @login_required(login_url='/cuentas/login/')
 def dashboard_request(request):
-    #dashboard_list = Tarea.objects.all()
-    #if request.method == "POST":
-       # if request.POST.get('deleteButton'):
-              #  deleteButtonItemValue=request.POST.getlist('deleteButton')
-               # obj = Tarea( id = deleteButtonItemValue[0]) 
-                #Tarea.objects.filter(id = deleteButtonItemValue[0]).update(enPapelera='True')
 
     estudiantes_list = Estudiante.objects.all()
     estudiante_actual = Estudiante.objects.get(user = request.user)
 
-    #if request.user.is_staff:
-    actividades_list = Actividad.objects.all()
-
+    #Desde aqui se procesa la barra de progreso de horas por estudiante
     my_actividades_list= Actividad.objects.raw('SELECT id, estudiante_id, horas, enPapelera FROM actividades_actividad where estudiante_id == '+ str(estudiante_actual.id)+" AND enPapelera==false")
     horasTotalesPorEstudiante=0      
-    #print(str(estudiante_actual.id))
     for actividad in my_actividades_list:
             horasTotalesPorEstudiante+= actividad.horas
 
     #horasTotalesPorEstudiante=30  #para pruebas
     porcentaje= (100 / 300) * horasTotalesPorEstudiante
     porcentajeWidth = int(porcentaje)
-    #print(horasTotalesPorEstudiante)
-    #print(porcentaje)
 
-    return render (request=request, template_name="../templates/dashboard.html", context={"progreso":horasTotalesPorEstudiante,"porcentaje":porcentaje,"width":porcentajeWidth,})
+
+    #Desde aqui se procesa la barra de progreso de dias del TCU por estudiante
+    current_datetime = datetime.date.today()
+    inicioTCU = estudiante_actual.fechaInicioTCU
+    finalTCU = estudiante_actual.fechaFinTCU
+
+    diasRestantesDelTCU =  finalTCU - current_datetime
+
+    diasDesdeInicioTCU = current_datetime - inicioTCU
+    diasTCU = diasDesdeInicioTCU.days
+
+    totalDiasTCU = 365
+
+    porcentajeDaysYear= (100 / totalDiasTCU) * diasTCU
+    porcentajeWidthDaysYear = int(porcentajeDaysYear)
+
+    return render (request=request, template_name="../templates/dashboard.html", context={"progreso":horasTotalesPorEstudiante,
+    "porcentaje":porcentaje,"width":porcentajeWidth,"diasTCU":diasTCU,"inicioTCU":inicioTCU,"finalTCU":finalTCU,"totalDiasTCU":totalDiasTCU,
+    "porcentajeDaysYear":porcentajeDaysYear,"porcentajeWidthDaysYear":porcentajeWidthDaysYear,})
