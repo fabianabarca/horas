702a64b861c967678f8c3ea3543051da2dbe4011
jmtv <jmtvlu@gmail.com>
2022-07-26 15:36:46 -0600

    botón de estudiantes a pagina de estudiantes funciona

--- estudiantes/views.py
+++ estudiantes/views.py
@@ -4,8 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from cuentas.models import *
 from horas.forms import EstudiantesForm
 from actividades.models import Actividad
-from proyectos.models import Proyecto
-import datetime
+from django.http import HttpResponse
 
 from django.contrib.auth.decorators import login_required
 
@@ -21,55 +20,11 @@ def estudiantes_request(request):
                 studentButtonItemValue=request.POST.getlist('studentButton')
                 estudianteAVer = Estudiante.objects.filter(id = studentButtonItemValue[0])
                 staffBotonVistaEstudiante = True
-                #response = HttpResponseRedirect("/")
-                #response.headers["estudiante"] = estudianteAVer[0].id
-                #return response
-                estudiante_actual = estudianteAVer[0]
-
-     
-                estudiantes_list = Estudiante.objects.all()
-
-                numeroEstudiantes=estudiantes_list.filter(user__is_staff=False).count
-
-                proyectos_list = Proyecto.objects.all()
-                numeroProyectos=proyectos_list.count
-                
-
-                #Desde aqui se procesa la barra de progreso de horas por estudiante
-                my_actividades_list= Actividad.objects.raw('SELECT id, estudiante_id, horas, enPapelera FROM actividades_actividad where estudiante_id == '+ str(estudiante_actual.id)+" AND enPapelera==false")
-                horasTotalesPorEstudiante=0      
-                for actividad in my_actividades_list:
-                    if actividad.estado == "A":
-                        horasTotalesPorEstudiante+= actividad.horas
-
-                #horasTotalesPorEstudiante=30  #para pruebas
-                porcentaje= (100 / 300) * horasTotalesPorEstudiante
-                porcentajeWidth = int(porcentaje)
-
-
-                #Desde aqui se procesa la barra de progreso de dias del TCU por estudiante
-                current_datetime = datetime.date.today()
-                inicioTCU = estudiante_actual.fechaInicioTCU
-                finalTCU = estudiante_actual.fechaFinTCU
-
-                diasRestantesDelTCU =  finalTCU - current_datetime
-
-                diasDesdeInicioTCU = current_datetime - inicioTCU
-                diasTCU = diasDesdeInicioTCU.days
-
-                totalDiasTCU = 365
-
-                porcentajeDaysYear= (100 / totalDiasTCU) * diasTCU
-                porcentajeWidthDaysYear = int(porcentajeDaysYear)
-
-
-                factorDeAvance =  porcentaje / porcentajeDaysYear
-                redondeadoFactorDeAvance = int(factorDeAvance)
-
-                return render (request=request, template_name="../templates/indexEstudiante.html", context={"progreso":horasTotalesPorEstudiante,
-                "porcentaje":porcentaje,"width":porcentajeWidth,"diasTCU":diasTCU,"inicioTCU":inicioTCU,"finalTCU":finalTCU,"totalDiasTCU":totalDiasTCU,
-                "porcentajeDaysYear":porcentajeDaysYear,"porcentajeWidthDaysYear":porcentajeWidthDaysYear,"factorDeAvance":factorDeAvance,
-                "numeroEstudiantes":numeroEstudiantes,"numeroProyectos":numeroProyectos,"estudiante_actual":estudiante_actual,})
+                response = HttpResponseRedirect("/")
+                response.headers["estudiante"] = estudianteAVer[0].id
+                user = estudianteAVer[0].user
+                return response
+                #return render (request=request, template_name="../../inicio/templates/index.html", context={"estudianteID":estudianteAVer[0].id,})
 
 
     estudiantes_list = Estudiante.objects.all().filter(user__is_staff=False)
--- inicio/urls.py
+++ inicio/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('index/<int:id>', views.index, name='index'),
+
 ]
