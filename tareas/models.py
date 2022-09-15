b3fa6a5492d3e9b2cd8772eec62c7127c567ad2f
Fabián Abarca <ensinergia@gmail.com>
2022-09-14 22:20:07 -0600

    Agregar estados de vigencia y urgencia a las tareas.

--- tareas/models.py
+++ tareas/models.py
@@ -8,11 +8,18 @@ from django.db import models
 # Create your models here.
 
 class Tarea(Registro):
+    ESTADOS = (
+        ('A', 'Activa'),
+        ('V', 'Vencida'),
+        ('F', 'Finalizada'),
+    )
     nombre = models.CharField(max_length=500)
     estudiante = models.ManyToManyField(Estudiante)
     tareaSuperior = models.ForeignKey('self', on_delete=models.SET_NULL,null=True,blank=True)
     objetivo = models.ForeignKey(Objetivo, on_delete=models.SET_NULL,null=True)
     descripcion = models.CharField(max_length=500)
+    estado = models.CharField(blank=True, null=True, choices=ESTADOS, max_length=1, default='A')
+    urgente = models.BooleanField(default=False)
 
     def __str__(self):
         return self.nombre
