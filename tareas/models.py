0525491858c2e5b9d1fc2e64be6c5a3d00930726
Fabián Abarca <ensinergia@gmail.com>
2022-09-14 22:47:09 -0600

    Agregar el creador de una tarea pero es necesario ajustar que sea el usuario logueado.

--- tareas/models.py
+++ tareas/models.py
@@ -1,10 +1,9 @@
 from proyectos.models import Objetivo
 from cuentas.models import Estudiante
 from inicio.models import Registro
+from django.contrib.auth.models import User
 from django.db import models
 from datetime import date
-
-
 
 # Create your models here.
 
@@ -16,13 +15,14 @@ class Tarea(Registro):
     )
     nombre = models.CharField(max_length=500)
     estudiante = models.ManyToManyField(Estudiante)
-    tareaSuperior = models.ForeignKey('self', on_delete=models.SET_NULL,null=True,blank=True)
-    objetivo = models.ForeignKey(Objetivo, on_delete=models.SET_NULL,null=True)
+    tareaSuperior = models.ForeignKey('self', on_delete=models.SET_NULL, null=True,blank=True)
+    objetivo = models.ForeignKey(Objetivo, on_delete=models.SET_NULL, null=True)
     descripcion = models.CharField(max_length=500)
     estado = models.CharField(blank=True, null=True, choices=ESTADOS, max_length=1, default='A')
     urgente = models.BooleanField(default=False)
     fecha_creacion = models.DateField(auto_now_add=True, blank=True, null=True)
     fecha_limite = models.DateField(blank=True, null=True)
+    creado_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nombre
