6a0ad741dfdf050952dcd22c7026bf2bff4f5e91
Fabián Abarca <ensinergia@gmail.com>
2022-09-14 22:04:28 -0600

    Agregar estados a estudiantes (como activo, finalizado, prórroga, etc.).

--- cuentas/models.py
+++ cuentas/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# Create your models here.
+
 class Carrera(models.Model):
     nombre = models.CharField(max_length=100)
 
@@ -23,11 +25,19 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class Estudiante(models.Model):
+    ESTADOS = (
+        ('A', 'Activo'),
+        ('F', 'Finalizado'),
+        ('P', 'Prórroga'),
+        ('R', 'Retirado'),
+        ('I', 'Incompleto'),
+    )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     tutor = models.ForeignKey(Profesor, blank=True, null=True, on_delete=models.SET_NULL)
     carrera = models.ForeignKey(Carrera, blank=True, null=True, on_delete=models.SET_NULL)
     fechaInicioTCU = models.DateField(blank=True, null=True)
     fechaFinTCU = models.DateField(blank=True, null=True)
+    estado = models.CharField(blank=False, null=True, choices=ESTADOS, max_length=1, default='A')
 
     def __str__(self):
         return self.user.first_name
