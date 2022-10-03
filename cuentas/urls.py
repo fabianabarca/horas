ce43d853a42d0501c225ccf33ba56d78631662d8
Fabián Abarca <ensinergia@gmail.com>
2022-10-02 23:30:31 -0600

    Actualización de base.html: nuevos links, eliminación de partes innecesarias, nuevo favicon, nuevos logos.

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views
 from django.urls import path
 
 urlpatterns = [
+    path('perfil/', perfil, name='perfil'),
     path('login/', login_request, name='login'),
     path('logout/', logout_request, name= 'logout'),
     path('register/', register_request, name='register')
