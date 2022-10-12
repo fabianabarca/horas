3800cc700e5ac427a087cdca8ca862e8365414ba
Fabián Abarca <ensinergia@gmail.com>
2022-10-12 09:04:56 -0600

    Ajustar formato de la barra de progreso de estudiantes en la lista y cambiar nombre de función de la vista de estudiantes (y su correspondiente url).

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -3,6 +3,6 @@ from django.contrib.auth import views
 from django.urls import path
 
 urlpatterns = [
-    path('', estudiantes_request, name='estudiantes'),
+    path('', estudiantes, name='estudiantes'),
     path('editar_estudiante/<int:id>', editar_estudiante, name='editar_estudiante'),
 ]
--- estudiantes/views.py
+++ estudiantes/views.py
@@ -9,23 +9,27 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='/cuentas/ingreso/')
-def estudiantes_request(request):
 
-    staffBotonVistaEstudiante= False
+
+@login_required(login_url='/cuentas/ingreso/')
+def estudiantes(request):
+    '''Recopila la información de todos los estudiantes
+    registrados para mostrarla en una lista.
+    '''
+    staffBotonVistaEstudiante = False
 
     if request.method == "POST":
-        list_of_inputs=request.POST.getlist('inputs')
+        list_of_inputs = request.POST.getlist('inputs')
         if request.POST.get('studentButton'):
-                studentButtonItemValue=request.POST.getlist('studentButton')
-                estudianteAVer = Estudiante.objects.filter(id = studentButtonItemValue[0])
-                staffBotonVistaEstudiante = True
-                response = HttpResponseRedirect("/")
-                response.headers["estudiante"] = estudianteAVer[0].id
-                user = estudianteAVer[0].user
-                return response
-                #return render (request=request, template_name="../../inicio/templates/index.html", context={"estudianteID":estudianteAVer[0].id,})
-
+            studentButtonItemValue = request.POST.getlist('studentButton')
+            estudianteAVer = Estudiante.objects.filter(
+                id=studentButtonItemValue[0])
+            staffBotonVistaEstudiante = True
+            response = HttpResponseRedirect("/")
+            response.headers["estudiante"] = estudianteAVer[0].id
+            user = estudianteAVer[0].user
+            return response
+            # return render (request=request, template_name="../../inicio/templates/index.html", context={"estudianteID":estudianteAVer[0].id,})
 
     estudiantes_list = Estudiante.objects.all().filter(user__is_staff=False)
     if request.user.is_staff:
@@ -35,34 +39,42 @@ def estudiantes_request(request):
     porcentajeEstudianteslist = []
     porcentajeWidthEstudianteslist = []
 
-    horasTotalesPorEstudiante=0
+    horasTotalesPorEstudiante = 0
     for estudiante in estudiantes_list:
-       
 
-        horasTotalesPorEstudiante=0
+        horasTotalesPorEstudiante = 0
         for actividad in actividades_list:
-            
+
             if actividad.estudiante.user.username == estudiante.user.username:
                 if actividad.estado == "A":
-                    horasTotalesPorEstudiante+= actividad.horas
+                    horasTotalesPorEstudiante += actividad.horas
 
         horasEstudianteslist.append(horasTotalesPorEstudiante)
-        porcentaje= (100 / 300) * horasTotalesPorEstudiante
+        porcentaje = round((100 / 300) * horasTotalesPorEstudiante)
         porcentajeEstudianteslist.append(porcentaje)
         porcentajeWidthEstudianteslist.append(int(porcentaje))
 
-    zipHoras= zip(estudiantes_list,horasEstudianteslist,porcentajeEstudianteslist,porcentajeWidthEstudianteslist)    
-    
-    return render (request=request, template_name="../templates/estudiantes.html", context={"zipHoras":zipHoras,"horaslist":horasEstudianteslist,
-    "estudiantes":estudiantes_list,"actividades":actividades_list,
-    "porcentajeList":porcentajeEstudianteslist,"porcentajeWidthList":porcentajeWidthEstudianteslist})
+    zipHoras = zip(estudiantes_list, horasEstudianteslist,
+                   porcentajeEstudianteslist, porcentajeWidthEstudianteslist)
+
+    context = {
+        "zipHoras": zipHoras,
+        "horaslist": horasEstudianteslist,
+        "estudiantes": estudiantes_list,
+        "actividades": actividades_list,
+        "porcentajeList": porcentajeEstudianteslist,
+        "porcentajeWidthList": porcentajeWidthEstudianteslist
+    }
+
+    return render(request, "estudiantes.html", context)
+
 
 @login_required(login_url='/cuentas/ingreso/')
 def editar_estudiante(request, id):
 
-    obj = get_object_or_404(Estudiante, id = id) 
+    obj = get_object_or_404(Estudiante, id=id)
 
-    form = EstudiantesForm(request.POST or None, instance = obj)
+    form = EstudiantesForm(request.POST or None, instance=obj)
     form.fields['user'].widget = forms.HiddenInput()
     form.fields['carrera'].widget = forms.HiddenInput()
     form.fields['fecha_inicio'].widget = forms.HiddenInput()
@@ -72,7 +84,4 @@ def editar_estudiante(request, id):
         form.save()
         return HttpResponseRedirect("/estudiantes")
 
- 
-    return render(request, "editar_estudiante.html", context={"estudiante_form":form})
-
-
+    return render(request, "editar_estudiante.html", context={"estudiante_form": form})
