b6604b4d47a756423a5cc500a693fa5ddf384a9a
Fabián Abarca <ensinergia@gmail.com>
2022-10-14 19:42:45 -0600

    Creación del mecanismo de cambio de contraseña (con herramientas de Django) en el perfil.

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,102 +1,120 @@
-from django.shortcuts import  render, redirect
+from django.shortcuts import render, redirect
 from horas.forms import *
 from django.contrib import messages
 from actividades.views import *
 from inicio.views import *
-from django.contrib.auth import login, authenticate, logout #add this
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from cuentas.models import *
 from django.core.exceptions import ValidationError
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 @staff_member_required(login_url='/cuentas/ingreso/')
 def register_request(request):
-	form = NewUserForm(request.POST or None)
-	if request.method == "POST":
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			
-			carrera = form.cleaned_data.get('carrera')
-			
-			if(not Carrera.objects.filter(nombre__contains=carrera).exists()):
-				
-				c = Carrera(nombre=carrera)
-				c.save()
+    form = NewUserForm(request.POST or None)
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
 
-			
-			#Actualmente en la base de datos crear usuario significa crearlo como estudiante
-			#aqui diferencia entre asignar valores al estudiante creado o asignar estudiante como profesor tambien
-			estudiante = Estudiante.objects.get(id=user.id)
-			estudiante.carrera_id = Carrera.objects.get(nombre__contains=carrera)
-			#estudiante.save()
-			fecha_inicio = form.cleaned_data.get('fecha_inicio')
-			fecha_final	= form.cleaned_data.get('fecha_final')
-			estudiante.fecha_inicio=fecha_inicio
-			estudiante.fecha_final=fecha_final
-			
-			estudiante.save()
+            carrera = form.cleaned_data.get('carrera')
 
+            if (not Carrera.objects.filter(nombre__contains=carrera).exists()):
 
-			is_staff = form.cleaned_data.get('is_staff')
-			
-			if ( is_staff):
-				user.is_staff=is_staff
-				user.is_superuser=is_staff
-				user.save()
+                c = Carrera(nombre=carrera)
+                c.save()
 
-				profesor = Profesor(user_id=user.id)
-				profesor.save()
+            # Actualmente en la base de datos crear usuario significa crearlo como estudiante
+            # aqui diferencia entre asignar valores al estudiante creado o asignar estudiante como profesor tambien
+            estudiante = Estudiante.objects.get(id=user.id)
+            estudiante.carrera_id = Carrera.objects.get(
+                nombre__contains=carrera)
+            # estudiante.save()
+            fecha_inicio = form.cleaned_data.get('fecha_inicio')
+            fecha_final = form.cleaned_data.get('fecha_final')
+            estudiante.fecha_inicio = fecha_inicio
+            estudiante.fecha_final = fecha_final
 
-			#login(request, user)
-			messages.success(request, "Registro de " +user.username+" exitoso." )
-			return redirect(register_request)
-		else:
-			messages.error(request, "Falló el registro. Información inválida.")
-	#form = NewUserForm()
+            estudiante.save()
 
-	context = {
-		"register_form":form,
-	}
+            is_staff = form.cleaned_data.get('is_staff')
 
-	return render (request, "registro.html", context)
+            if (is_staff):
+                user.is_staff = is_staff
+                user.is_superuser = is_staff
+                user.save()
+
+                profesor = Profesor(user_id=user.id)
+                profesor.save()
+
+            #login(request, user)
+            messages.success(request, "Registro de " +
+                             user.username+" exitoso.")
+            return redirect(register_request)
+        else:
+            messages.error(request, "Falló el registro. Información inválida.")
+    #form = NewUserForm()
+
+    context = {
+        "register_form": form,
+    }
+
+    return render(request, "registro.html", context)
 
 
 def login_request(request):
 
-	form = CustomAuthenticationForm(request.POST or None)
-	if request.method == "POST":
-		form = CustomAuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				#messages.info(request, "You are now logged in as {username}.")
-				
-				return redirect(index)
-			else:
-				
-				messages.error(request,"Usuario o contraseña inválido.")
-		else:
-			messages.error(request,"Usuario o contraseña inválido.")
-	form = CustomAuthenticationForm()
-	return render(request, "ingreso.html", context={"login_form":form})
+    form = CustomAuthenticationForm(request.POST or None)
+    if request.method == "POST":
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                #messages.info(request, "You are now logged in as {username}.")
+
+                return redirect(index)
+            else:
+
+                messages.error(request, "Usuario o contraseña inválido.")
+        else:
+            messages.error(request, "Usuario o contraseña inválido.")
+    form = CustomAuthenticationForm()
+    return render(request, "ingreso.html", context={"login_form": form})
+
 
 def logout_request(request):
-	logout(request)
-	messages.info(request, "La sesión ha terminado exitosamente.") 
-	return redirect(login_request)
+    logout(request)
+    messages.info(request, "La sesión ha terminado exitosamente.")
+    return redirect(login_request)
 
 
 @login_required(login_url='/cuentas/ingreso/')
 def perfil(request):
 
+    # Datos de estudiante actual
     estudiante_actual = Estudiante.objects.get(user=request.user)
 
+    # Cambio de contraseña
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request,
+                             'La contraseña ha sido cambiada exitosamente',
+                             extra_tags='alert-success')
+        return redirect('perfil')
+
+    else:
+        form = PasswordChangeForm(request.user)
+
     context = {
-		'estudiante': estudiante_actual
-	}
+        'estudiante': estudiante_actual,
+        'form': form,
+    }
     return render(request, 'perfil.html', context)
