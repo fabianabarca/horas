50480cdedd88950024187d54d9df4d1c9621c8fc
Manuel Fernández <mgnr7@hotmail.com>
2022-10-17 17:38:25 -0600

    Se crea el grafico de barras con las horas de los ultimos 7 dias

--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,5 @@
 import json
+from tkinter import W
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -10,6 +11,7 @@ from actividades.models import Actividad
 from cuentas.models import Estudiante
 from proyectos.models import Proyecto
 import datetime
+from datetime import timedelta
 
 # Create your views here.
 
@@ -150,6 +152,72 @@ def index(request, id=9999):
                 horasPorDia[numeroActividades][2]=actividad.fecha.day
                 horasPorDia[numeroActividades][3]=actividad.horas
                 numeroActividades+=1
+    
+
+        
+        #[Calcular la cantidad de horas semanales de cada estudiante]
+
+        #Calcular el ultimo dia de los 7 de la semana, (dia actual - 7)
+        last_date = current_datetime - timedelta(days=6)
+        last_day = last_date.day
+
+        #Arreglo para almacenar las actividades semanales
+        weekly_activities = []
+        activity_index = 0
+        for actividad in horasPorDia:
+            if(horasPorDia[activity_index][0] == current_datetime.year and (horasPorDia[activity_index][1] == current_datetime.month or horasPorDia[activity_index][1] == current_datetime.month-1) ):
+                if(horasPorDia[activity_index][2] >= last_day and horasPorDia[activity_index][2] <= current_datetime.day):
+                    weekly_activities.append(horasPorDia[activity_index])
+            activity_index+=1
+
+        #Ordenar weekly_activities por dia
+        weekly_activities.sort()
+        
+        #Para imprimir y ver que todo esta bien
+        '''
+        i = 0
+        for act in weekly_activities:
+            print("Semana:")
+            print(weekly_activities[i])
+            i+=1
+        '''
+
+        #Almacena la cantidad de horas por cada uno de los 7 ultimos dias
+        #Inicializa todos los valores en 0
+        weekly_hours = [0] * 7
+        #indice para el arreglo weekly_hours
+        i = 0
+        #indice para weekly activities
+        j = 0
+        
+        #Para control
+        pr_day = last_day
+        for activity in weekly_activities:
+            if(pr_day != weekly_activities[j][2]):
+                i += (weekly_activities[j][2] - pr_day)
+            
+            weekly_hours[i]+= weekly_activities[j][3]
+            pr_day = weekly_activities[j][2]
+            j+=1
+
+
+        #Almacena el nombre de los dias
+        week_days = []
+        #Arreglo con los nombres de los dias de la semana
+        days_names = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+        #Se obtiene el numero del utimo dia en la semana
+        day_of_week = last_date.weekday()
+
+        days_index = day_of_week
+
+        while (days_index <= 6):
+            week_days.append(days_names[days_index])
+            days_index += 1
+            if(days_index == 7):
+                days_index = 0
+            if(days_index == day_of_week):
+                break
+
 
         inicio = 'resumen.html'
         context = {
@@ -160,6 +228,8 @@ def index(request, id=9999):
             "indiceAvanceW":indiceAvanceW,
             "horasPorDia":json.dumps(horasPorDia),
             "numeroActividades":numeroActividades,
+            "horas_semanales": json.dumps(weekly_hours),
+            "dias_semana": json.dumps(week_days)
         }
 
     return render(request, inicio, context)
