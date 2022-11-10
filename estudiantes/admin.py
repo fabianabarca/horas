85f926fbc6031c0bfd257cea76c9632ab6acdd27
Fabián Abarca <ensinergia@gmail.com>
2022-11-10 09:17:36 -0600

    Creación de modelo de equipos dentro de la app estudiantes e incorporación en el panel de administración.

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,3 +1,6 @@
+from .models import *
 from django.contrib import admin
 
-# Register your models here.
+# Register your models to admin site, then you can add, edit, delete and search your models in Django admin site.
+
+admin.site.register(Equipo)
--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from cuentas.models import Estudiante
+from proyectos.models import Proyecto
 
 # Create your models here.
+
+
+class Equipo(models.Model):
+    '''Modelo de un equipo de estudiantes.
+
+    Contiene el nombre, participantes y otras
+    informaciones de cada equipo.
+    '''
+    ESTADOS = (
+        ('A', 'Activo'),
+        ('I', 'Inactivo'),
+        ('F', 'Finalizado'),
+    )
+    nombre = models.CharField(max_length=64, blank=False, null=False)
+    descripcion = models.TextField(blank=True, null=True)
+    estudiantes = models.ManyToManyField(Estudiante, blank=True, null=True)
+    proyectos = models.ManyToManyField(Proyecto, blank=True, null=True)
+    contacto = models.CharField(max_length=128, blank=True, null=True)
+    estado = models.CharField(choices=ESTADOS, max_length=1, default='A', blank=False, null=True)
+
+    def __str__(self):
+        return self.nombre
